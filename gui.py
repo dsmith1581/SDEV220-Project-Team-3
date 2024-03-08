3bd381b8a211d65a7c4f46d455beff4ea56856de
dsmith1581 <124409577+dsmith1581@users.noreply.github.com>
2024-03-08 21:27:46 +0000

    Add details view
    Adds interface elements to select the desired asset and view its details

--- gui.py
+++ gui.py
@@ -63,11 +63,29 @@ def campus_view(campus):
     #building_menu = ttk.OptionMenu(root_window, tkinter.StringVar(value=current_building_name()), *building_names)
     building_menu = ttk.OptionMenu(root_window, building_selection, building_selection.get(), *building_names)
 
-    # Inventory add/remove buttons
-    inventory_label = ttk.Label(root_window, text = "Inventory:")
-    inventory_add_button = ttk.Button(root_window, text = "Add", command=add_inventory_workflow)
-    inventory_remove_button = ttk.Button(root_window, text = "Remove", command=remove_inventory_workflow)
+    # Spinbox for inventory ID selector
+    max_value = len(current_building.get_items())
+    if max_value < 1:
+            max_value = 1
+    inventory_spinbox = ttk.Spinbox(root_window, from_=1, to=max_value, width=5)
+    inventory_spinbox.set(1)
 
+    # Inventory buttons
+    inventory_label = ttk.Label(root_window, text = "Inventory ID:")
+    inventory_add_button = ttk.Button(root_window, text = "Add", command=add_inventory_workflow)
+    # The details button needs to call the inventory_details function with arguments
+    def inventory_details_helper():
+        # Skip when we have no items in an inventory yet
+        if len(current_building.get_items()) == 0:
+            return
+        # Find some info about what the current asset is
+        current_index = int(inventory_spinbox.get())
+        asset = current_building.get_items()[current_index]
+        # Now call the actual inventory details function
+        inventory_details(asset)
+
+    inventory_details_button = ttk.Button(root_window, text = "Details", command=inventory_details_helper)
+    
     # Asset info output area
     asset_info = tkinter.scrolledtext.ScrolledText(root_window, font=("Courier", 10))
     asset_info.insert(tkinter.END, asset_details)
@@ -88,13 +106,14 @@ def campus_view(campus):
     root_window.grid_columnconfigure(2, weight = 1)
 
     # Place items in grid
-    campus_label.grid(           row = 0, column = 0, columnspan = 3, padx = (5, 0), pady = (0, 10), sticky = "w")
-    back_button.grid(            row = 0, column = 3, columnspan = 1, padx = (0, 5), pady = (10, 0), sticky = "nw")
-    building_menu.grid(          row = 1, column = 0, columnspan = 4, padx = (5, 0), pady = (0, 10), sticky = "w")
-    inventory_label.grid(        row = 2, column = 0, columnspan = 1, padx = (5, 0), pady = (0, 10), sticky = "w")
-    inventory_add_button.grid(   row = 2, column = 1, columnspan = 1, padx = (5, 0), pady = (0, 10), sticky = "w")
-    inventory_remove_button.grid(row = 2, column = 2, columnspan = 1, padx = (5, 0), pady = (0, 10), sticky = "w")
-    asset_info.grid(             row = 3, column = 0, columnspan = 4, padx = (5, 5), pady = (0,  5), sticky = "nsew")
+    campus_label.grid(            row = 0, column = 0, columnspan = 3, padx = (5, 0), pady = (0, 10), sticky = "w")
+    back_button.grid(             row = 0, column = 3, columnspan = 1, padx = (0, 5), pady = (10, 0), sticky = "nw")
+    building_menu.grid(           row = 1, column = 0, columnspan = 3, padx = (5, 0), pady = (0, 10), sticky = "w")
+    inventory_label.grid(         row = 2, column = 0, columnspan = 1, padx = (5, 0), pady = (0, 10), sticky = "w")
+    inventory_spinbox.grid(       row = 2, column = 1, columnspan = 1, padx = (5, 0), pady = (0, 10), sticky = "w")
+    inventory_details_button.grid(row = 2, column = 2, columnspan = 1, padx = (5, 0), pady = (0, 10), sticky = "w")
+    inventory_add_button.grid(    row = 2, column = 3, columnspan = 1, padx = (5, 0), pady = (0, 10), sticky = "w")
+    asset_info.grid(              row = 3, column = 0, columnspan = 4, padx = (5, 5), pady = (0,  5), sticky = "nsew")
 
     # Handle campus selection changes
     def on_building_selection_change(*args):
@@ -106,6 +125,16 @@ def campus_view(campus):
         asset_details = assets_to_string(current_building.get_items())
         asset_info.delete(1.0, tkinter.END)
         asset_info.insert(tkinter.END, asset_details)
+
+        # Update spinbox range and reset to the default value
+        max_value = len(current_building.get_items())
+        if max_value < 1:
+            max_value = 1
+
+        print(max_value)
+
+        inventory_spinbox.config(from_=1, to=max_value)
+        inventory_spinbox.set(1)
 
         return
 
@@ -138,6 +167,16 @@ def create_window():
 
     # Run the application
     root_window.mainloop()
+
+    return
+
+# Popup window to see the details of an asset
+def inventory_details(asset):
+    # Gather the asset details
+    details = f"Name: {asset.get_item_name()}\nQuantity in room: {asset.get_room_quantity()}\nDescription: {asset.get_description()}\nDepartment: {asset.get_department()}\nBuilding: {asset.get_from_building()}\nManufacturer: {asset.get_manufacturer()}"
+    
+    # Show them to the user
+    tkinter.messagebox.showinfo(f"{asset.get_item_name()} - Detailed Information", details)
 
     return
 
@@ -203,13 +242,6 @@ def numbered_lines(string):
     result =  "\n".join(f"{str(i+1).rjust(max_width)}.  {line}" for i, line in enumerate(lines))
 
     return result
-
-# Popup window workflow to delete an inventory item
-def remove_inventory_workflow():
-    # TODO - implement inventory workflow modal
-    alert_unimplemented()
-
-    return
 
 # Handles selecting a file and loading the inventory from that
 def select_and_load_file():
